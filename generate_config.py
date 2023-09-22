51dea9b8f98517c1048a697acf061a01204f02f5
Sebastian Kutter <sebastian@skutter.de>
2023-09-23 01:06:13 +0200

    URL validation

--- generate_config.py
+++ generate_config.py
@@ -6,9 +6,9 @@ import argparse
 import sys
 import secrets
 
-for package in ["jinja2", "rich"]:
+for package in ["jinja2", "rich", "validators"]:
     try:
-        pkg_resources.get_distribution("jinja2")
+        pkg_resources.get_distribution(package)
     except pkg_resources.DistributionNotFound:
         subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
@@ -17,22 +17,24 @@ from rich import print
 from rich.console import Console
 from rich.markdown import Markdown
 from rich.prompt import Prompt, IntPrompt
+import validators
 
 with open("templates/docker-compose.override.yml.j2") as template_file:
     template = Template(template_file.read(), trim_blocks=True, lstrip_blocks=True)
 
 parser = argparse.ArgumentParser(prog=__file__, description="Generate your docker-compose.override.yml for Logto")
 
-parser.add_argument("-e", "--endpoint-url", help="URL for the \"default\" authentication endpoint")
+parser.add_argument("-e", "--endpoint-url", required=True, help="URL for the \"default\" authentication endpoint")
 parser.add_argument("-a", "--admin-endpoint-url", help="URL for the administration endpoint")
 parser.add_argument("-p", "--endpoint-port", type=int, default=3001, help="Port where the \"default\" authentication endpoint should be exposed to\nThe port also needs to be included in the URL if Logto will only be reachable there\nDefault: 3001")
 parser.add_argument("-P", "--admin-endpoint-port", type=int, default=3002, help="Port where the administration endpoint should be exposed to\nThe port also needs to be included in the URL if Logto will only be reachable there\nDefault: 3002")
 parser.add_argument("-d", "--postgres-data-dir", default=None, help="Path to the directory where Postgres should store it\'s data.\nDefault: Docker volume default")
-args = parser.parse_args()
 
-# dict for templating data
 data = {}
 if len(sys.argv) > 1:
+    args = parser.parse_args()
+
+# dict for templating data
     # Populate data from args into "data" for templating
     for k, v in vars(args).items():
         data[k] = v
@@ -41,8 +43,18 @@ else:
     intro = Markdown("# Welcome to Logto!\n\n This script will help you generate a docker-compose override file to get started. For this, a few questions will be asked.  \nLet\'s get started!")
     console.print(intro)
     console.line(2)
-    data["endpoint_url"] = Prompt.ask(prompt="[yellow]Where should Logto be reachable at? [/][magenta]\[eg. https://sso.example.com\][/]", console=console)
-    data["admin_endpoint_url"] = Prompt.ask(prompt="[yellow]Where should the Logto administration interface be reachable at? [/][magenta]\[eg. https://sso-admin.example.com\][/]", console=console)
+    while not data.get("endpoint_url"):
+        prompt_input = Prompt.ask(prompt="[yellow]Where should Logto be reachable at? [/][magenta]\[eg. https://sso.example.com][/]", console=console)
+        if validators.url(prompt_input):
+            data["endpoint_url"] = prompt_input
+        else:
+            print("[red]This URL does not seem to be valid, please try again")
+    while not data.get("admin_endpoint_url"):
+        prompt_input = Prompt.ask(prompt="[yellow]Where should the Logto administration interface be reachable at? [/][magenta]\[eg. https://sso-admin.example.com][/]", console=console)
+        if validators.url(prompt_input):
+            data["admin_endpoint_url"] = prompt_input
+        else:
+            print("[red]This URL does not seem to be valid, please try again")
     data["endpoint_port"] = IntPrompt.ask(prompt="[yellow]Which port should Logto be exposed on?", console=console, default=3001)
 data["postgres_password"] = secrets.token_hex(32)
 print(template.render(data))
